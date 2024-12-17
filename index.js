08afe0fe90e43956dcd51374a2d0cd057f9e52a4
aliadibpour <ali.adp0101@gmail.com>
2024-12-17 22:20:11 +0330

    ffhirteen

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const io = new Server(server ,{
 })
 io.on('connection', async (socket) => {
   const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
