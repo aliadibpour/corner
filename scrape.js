482f3ad493f14d62413f54a6a5d259588503eacd
aliadibpour <ali.adp0101@gmail.com>
2024-12-21 16:00:34 +0330

    scar

--- scrape.js
+++ scrape.js
@@ -10,32 +10,31 @@ const scrapeLogic = async (res) => {
       "--no-zygote",
     ],
     executablePath:
-         process.env.PUPPETEER_EXECUTABLE_PATH
-        || puppeteer.executablePath(),
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
   });
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://example.com/");
+    await page.goto("https://developer.chrome.com/");
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
     // Type into search box
-    //await page.type(".search-box__input", "automate beyond recorder");
+    await page.type(".search-box__input", "automate beyond recorder");
 
     // Wait and click on first result
-    // const searchResultSelector = ".search-box__link";
-    // await page.waitForSelector(searchResultSelector);
-    //await page.click(searchResultSelector);
+    const searchResultSelector = ".search-box__link";
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
 
     // Locate the full title with a unique string
-    // const textSelector = await page.waitForSelector(
-    //   "text/Customize and automate"
-    // );
-    const fullTitle = await page.evaluate(() => {
-        return document.querySelectorAll("h1")[0].innerHTML
-    });
+    const textSelector = await page.waitForSelector(
+      "text/Customize and automate"
+    );
+    const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
     // Print the full title
     const logStatement = `The title of this blog post is ${fullTitle}`;
