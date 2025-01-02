a8b83a7c3f50efd70209535c6936ad31f3a39d5d
aliadibpour <ali.adp0101@gmail.com>
2025-01-02 09:06:31 +0330

    personal

--- a.js
+++ a.js
@@ -1,9 +1,15 @@
-const puppeteer = require("puppeteer");
 require("dotenv").config();
+const puppeteer = require('puppeteer-core');
 
-const a = async (res) => {
-  console.log("aa");
-  res.send("aaaaaaaaaa")
+const a = async (browser, evaluate, url) => {
+    const page = await browser.newPage();
+    // await page.setViewport({width: 1080, height: 1024});
+    page.setDefaultNavigationTimeout( 90000 );
+    await page.goto(url);
+    await page.waitForSelector('.Sections_container__03lu8' || ".EmptyStage_container__oSUiQ")
+    console.log('A user connected');
+    
+    return await evaluate(page)
 };
 
 module.exports = { a };
